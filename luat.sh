199d3a4ad91f9ba35bc0f0bfa2a6cb7064f08643
Bui The Phong <53887499+phongbui83@users.noreply.github.com>
2019-12-04 08:22:37 +0700

    Update luat.sh

--- luat.sh
+++ luat.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-scp phongbt@10.42.69.80:/root/INBOX /var/www/html/eml/
+scp phongbt@10.42.69.80:/root/INBOX/*.eml /var/www/html/eml/
 for f in *.eml; do cat $f | /usr/bin/ruby /var/www/html/rdm-mailhandler.rb  --url=http://lv-skms.chip1stop.com/redmine/ --key-file /etc/postfix/redmine-api.key --unknown-user=accept --no-permission-check --no-check-certificate-p Unyou --tracker=MailAlert --status=報告済・対応不要 --assigned-to=admin  --priority=normal ; done
 rm -rf /var/www/html/eml/*.eml
